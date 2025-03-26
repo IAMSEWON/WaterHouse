679c315baac935b9e58dc0c1621889ae2d61ee2e
Kobe <i3brooooo@gmail.com>
2025-03-26 19:06:26 +0900

    deploy - 배포 에러 수정

--- src/data/main.ts
+++ src/data/main.ts
@@ -1,24 +1,24 @@
-export const mainData: [{
+export const mainData: Array<{
     title: string;
     descriptionTitle: string;
     backgroundImage: string;
     description: string;
     moreLink: string;
     titleImage?: string;
-}] = [
+}> = [
         {
             title: "Bukchon Hanok Village",
             descriptionTitle: "Bukchon Hanok Village",
             description: "우리는 이곳에서 15세기부터 켜켜이 누적되어 온 시간의 묵직한 가치를 발견했습니다. 그 여정은 오랜 세월 동안 켜켜이 쌓이고 흔적으로 남은 시간을 있는 그대로 드러내는 것에서 시작합니다. 성급하게 가치를 판단하기 보다 기존의 흔적 위에 새롭게 펼쳐질 삶의 모습을 덧입히는 방식으로 접근했습니다.",
             backgroundImage: "/images/main/bukchon_hanok_village.png",
-            moreLink: "void"
+            moreLink: ""
         },
         {
             title: "적재건축",
             descriptionTitle: "The Story of Space with 적재건축",
             description: "한옥은 시간과 삶의 흔적이 쌓여 만들어진 살아있는 공간입니다. 적재건축은 과거의 유산을 계승하면서도 현대적 삶의 요구를 충족시키는 공간에 대해 고민했습니다. 마음에 안정과 평온함을 가져다주고, 자연과의 긴밀한 연결로 깊은 영감을 전하는 더 워터하우스는 건축가의 시선으로 해석한 특별한 공간적 경험을 제안합니다.",
             backgroundImage: "/images/main/jeogjae_architect.png",
-            moreLink: "void"
+            moreLink: ""
         },
         {
             title: "",
@@ -29,21 +29,21 @@ export const mainData: [{
             우리는 시대를 초월하는 디자인과 장인 정신을 바탕으로 한 라이프스타일을 추구하는 스칸디나비아 브랜드 &Tradition의 가구를 통해 절제된 아름다움, 깊이 있는 디테일, 그리고 편안한 배려가 담긴 공간 경험을 선보입니다.
             `,
             backgroundImage: "/images/main/tradition.png",
-            moreLink: "void"
+            moreLink: ""
         },
         {
             title: "Inspired by Experience",
             descriptionTitle: "Inspired by Experience",
             description: "더 워터하우스에서의 경험은 단순한 머무름을 넘어섭니다. 사색과 휴식을 위한 순간, 영감을 불러일으키는 특별한 경험들이 기다리고 있습니다. 공간 속에서 깊이 머물며 감각을 깨우고 새로운 가능성을 마주하는 다채로운 프로그램을 만나보세요.",
             backgroundImage: "/images/main/experience.png",
-            moreLink: "void"
+            moreLink: ""
         },
         {
             title: "Art in Our Space",
             descriptionTitle: "Art in Our Space",
             description: "더 워터하우스는 엄선한 한국 작가들의 작품이 시기별 흐름에 따라 새롭게 채워집니다. 머무는 사람들의 안목에 부응하는 웰 메이드 작품들은 공간에 창의적인 활력을 불어넣으며 분위기를 전환합니다. 잘 만든 공간, 한 폭의 작품 같은 아름다운 자연, 결정적 피스의 어울림을 통해 저마다의 흐름을 찾고 영감을 마주하는 시간이 되길 바랍니다.",
             backgroundImage: "/images/main/art_space.png",
-            moreLink: "void"
+            moreLink: ""
         }
     ];
 
